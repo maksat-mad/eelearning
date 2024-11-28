ccc5fef5ffe9af7f2281bfdb62847e5a51383c57
Maksat <maksat.madeniyetov@gmail.com>
2024-11-28 09:23:52 +0500

    remove loader in login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,11 +3,10 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatProgressSpinnerModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   standalone: true,
