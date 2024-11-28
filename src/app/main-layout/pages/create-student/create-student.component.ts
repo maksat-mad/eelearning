6cb5a4e5effd802c5a02c57bd1c76c8a49b03f8b
Maksat <maksat.madeniyetov@gmail.com>
2024-11-28 09:56:50 +0500

    create create student page

--- src/app/main-layout/pages/create-student/create-student.component.ts
+++ src/app/main-layout/pages/create-student/create-student.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-student',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create-student.component.html',
   styleUrl: './create-student.component.scss',
   standalone: true,
