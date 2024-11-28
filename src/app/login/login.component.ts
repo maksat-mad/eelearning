d09a24542d9dc591662b656f339c733085e8e611
Maksat <maksat.madeniyetov@gmail.com>
2024-11-28 09:11:26 +0500

    create login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatProgressSpinnerModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   standalone: true,
