fcb1c5dcf7c9c0bb8ae42ea1f546d04cc3b45db1
Maksat <maksat.madeniyetov@gmail.com>
2024-11-28 10:14:07 +0500

    create create course page

--- src/app/main-layout/pages/create-course/create-course.component.ts
+++ src/app/main-layout/pages/create-course/create-course.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-course',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create-course.component.html',
   styleUrl: './create-course.component.scss',
   standalone: true,
